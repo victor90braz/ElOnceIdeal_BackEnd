65e68b71ae8aeb3b4f0801d6941b1da4ef4266fe
victor90braz <victor.90braz@gmail.com>
2022-05-31 17:18:44 +0200

    Tested userRegister and custom Error

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const userRouter = require("./routers/userRouter");
+const { notFoundError, generalError } = require("../middlewares/errors");
 
 const app = express();
 
@@ -12,5 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
