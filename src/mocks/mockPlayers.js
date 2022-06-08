017db4f38e3ee28f245681f65550402daf12b73f
victor90braz <victor.90braz@gmail.com>
2022-06-08 16:50:59 +0200

    Refactored mockPlayers array

--- src/mocks/mockPlayers.js
+++ src/mocks/mockPlayers.js
@@ -16,40 +16,6 @@ const mockPlayers = [
     def: "88",
     phy: "99",
   },
-  {
-    id: "2",
-    name: "Buffon",
-    username: "bufon",
-    perfil: "Italia",
-    date: "18/06/1990",
-    nationality: "Brasil",
-    position: "Delantero",
-    image:
-      "https://phantom-elmundo.unidadeditorial.es/d1768bcbe2e6082856d1337df7a…",
-    pac: "66",
-    sho: "55",
-    pass: "55",
-    dri: "45",
-    def: "88",
-    phy: "99",
-  },
-  {
-    id: "3",
-    name: "Ricardo",
-    username: "Kaka",
-    perfil: "Brasil",
-    date: "18/06/1990",
-    nationality: "Brasil",
-    position: "Delantero",
-    image:
-      "https://phantom-elmundo.unidadeditorial.es/d1768bcbe2e6082856d1337df7a…",
-    pac: "66",
-    sho: "55",
-    pass: "55",
-    dri: "45",
-    def: "88",
-    phy: "99",
-  },
 ];
 
 const mockPlayerInformation = {
